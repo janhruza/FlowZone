78ad18c83bdeec12db3a129c0f2ee92d2f65f619
Jan Hrůza <honza.hruza.d5@gmail.com>
2025-01-23 16:39:21 +0100

    Added a new project: Expando

---
+++ Expando/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Expando
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Expando.MainWindow mw = new Expando.MainWindow();
+            MainWindow = mw;
+            MainWindow.Show();
+        }
+    }
+
+}
---
+++ Expando/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Expando;
+
+/// <summary>
+/// Representing the main window class.
+/// </summary>
+public partial class MainWindow : Window
+{
+    /// <summary>
+    /// Creates a new instance of the <see cref="MainWindow"/> class.
+    /// </summary>
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
