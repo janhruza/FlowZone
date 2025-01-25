93f025fd4c0f421580706c9fd7ca8539969db516
Jan Hrůza <honza.hruza.d5@gmail.com>
2025-01-25 14:07:12 +0100

    Added new pages definitions.

---
+++ Expando/Pages/PgExpanses.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Expando.Pages
+{
+    /// <summary>
+    /// Interaction logic for PgExpanses.xaml
+    /// </summary>
+    public partial class PgExpanses : Page
+    {
+        public PgExpanses()
+        {
+            InitializeComponent();
+        }
+    }
+}
