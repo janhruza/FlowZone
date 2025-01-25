d2310d53da8d2c7f126ab4727d6cf0ba82620bba
Jan Hrůza <honza.hruza.d5@gmail.com>
2025-01-25 15:49:47 +0100

    Expando: Added a profile page definition.

---
+++ Expando/Pages/PgProfile.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace Expando.Pages;
+
+/// <summary>
+/// Representing a user profile page (not a profile selection page).
+/// </summary>
+public partial class PgProfile : Page
+{
+    /// <summary>
+    /// Creates a new instance of the <see cref="PgProfile"/> class.
+    /// </summary>
+    public PgProfile()
+    {
+        InitializeComponent();
+    }
+}
