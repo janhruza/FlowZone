98ce91a7080f25ec710da1b3d6073de1b1c72eeb
Jan Hrůza <honza.hruza.d5@gmail.com>
2025-02-08 12:47:01 +0100

    Added a new project: RipTide

---
+++ RipTide/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace RipTide;
+
+/// <summary>
+/// Representing the main application class.
+/// </summary>
+public partial class App : Application
+{
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        MainWindow mw = new MainWindow
+        {
+            Title = App.Title
+        };
+
+        MainWindow = mw;
+        MainWindow.Show();
+    }
+
+    /// <summary>
+    /// Representing the main window title
+    /// </summary>
+    public const string Title = "RipTide";
+}
---
+++ RipTide/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace RipTide;
+
+/// <summary>
+/// Representing the main window class.
+/// </summary>
+public partial class MainWindow : Window
+{
+    /// <summary>
+    /// Creates a new instance of the <see cref="MainWindow"/> class.
+    /// </summary>
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void btnClose_Click(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+}
