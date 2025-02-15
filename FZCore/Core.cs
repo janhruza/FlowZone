8f90a075c206c691f0e2a0d2246c117cb2749c8a
Jan Hrůza <honza.hruza.d5@seznam.cz>
2025-02-15 19:57:32 +0000

    FZCore: Added some core functionality.

--- FZCore/Core.cs
+++ FZCore/Core.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using FZCore.Windows;
 
@@ -34,4 +35,57 @@ public static class Core
         _ = new LogViewer(Log.Path).ShowDialog();
         return true;
     }
+
+    /// <summary>
+    /// Shows the license for all the products inside of the FlowZone suite.
+    /// </summary>
+    /// <returns></returns>
+    public static bool ShowLicense()
+    {
+        InfoBox(FZData.License, FZData.Solution);
+        return true;
+    }
+
+#pragma warning disable WPF0001
+    private static Dictionary<FZThemeMode, ThemeMode> ThemesByNames => new Dictionary<FZThemeMode, ThemeMode>
+    {
+        {FZThemeMode.None, ThemeMode.None },
+        {FZThemeMode.Light, ThemeMode.Light },
+        {FZThemeMode.Dark, ThemeMode.Dark },
+        {FZThemeMode.System, ThemeMode.System }
+    };
+
+    /// <summary>
+    /// Sets the desired <paramref name="theme"/> to the desired <paramref name="window"/> (<see cref="Window"/> only scope).
+    /// </summary>
+    /// <param name="window">Target window.</param>
+    /// <param name="theme">Desired window theme.</param>
+    public static void SetWindowTheme(Window? window, FZThemeMode theme)
+    {
+        if (window == null)
+        {
+            return;
+        }
+
+        window.ThemeMode = ThemesByNames[theme];
+        return;
+    }
+
+    /// <summary>
+    /// Sets the desired <paramref name="theme"/> for the entire <paramref name="application"/> (full <see cref="Application"/> scope).
+    /// </summary>
+    /// <param name="application">Target application.</param>
+    /// <param name="theme">Desired application theme.</param>
+    public static void SetApplicationTheme(Application? application, FZThemeMode theme)
+    {
+        if (application == null)
+        {
+            return;
+        }
+
+        application.ThemeMode = ThemesByNames[theme];
+        return;
+    }
+
+#pragma warning restore WPF0001
 }
---
+++ FZCore/FZThemeMode.cs
@@ -0,0 +1,27 @@
+﻿namespace FZCore;
+
+/// <summary>
+/// Representing an enumeration of the avalable WPF themes.
+/// </summary>
+public enum FZThemeMode : byte
+{
+    /// <summary>
+    /// No custom theme.
+    /// </summary>
+    None    = 0,
+
+    /// <summary>
+    /// Light Fluent theme.
+    /// </summary>
+    Light   = 1,
+
+    /// <summary>
+    /// Dark Fluent theme.
+    /// </summary>
+    Dark    = 2,
+
+    /// <summary>
+    /// Representing the system theme, either <see cref="Light"/> or <see cref="Dark"/> based on your operating system configuration.
+    /// </summary>
+    System  = 3
+}
