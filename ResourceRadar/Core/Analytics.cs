4cfe5a78ee7b1f00d9f70d14414bd85a1b3e80ae
Jan Hrůza <honza.hruza.d5@seznam.cz>
2025-02-18 21:28:35 +0000

    ResourceRadar: Added more methods into the Analytics class and adjusted some names.

--- ResourceRadar/Core/Analytics.cs
+++ ResourceRadar/Core/Analytics.cs
@@ -21,7 +21,7 @@ public class Analytics
     /// </summary>
     /// <param name="collection"></param>
     /// <returns></returns>
-    public static Task<InventoryItemsCollection> ExpiredItemsAsync(InventoryItemsCollection collection)
+    public static Task<InventoryItemsCollection> ExpiredAsync(InventoryItemsCollection collection)
     {
         return Task.Run<InventoryItemsCollection>(() =>
         {
@@ -62,7 +62,7 @@ public class Analytics
     /// <param name="collection">Collection of items.</param>
     /// <param name="daysThreshold">Number of dates ahead of today that is considered being about to expire.</param>
     /// <returns>Líst of all items that are about to expire.</returns>
-    public static Task<InventoryItemsCollection> NearExpiredItemsAsync(InventoryItemsCollection collection, int daysThreshold)
+    public static Task<InventoryItemsCollection> NearExpiredAsync(InventoryItemsCollection collection, int daysThreshold)
     {
         return Task.Run<InventoryItemsCollection>(() =>
         {
@@ -92,7 +92,7 @@ public class Analytics
     /// <param name="collection">Collection of items.</param>
     /// <param name="topN">Number of items to return.</param>
     /// <returns>Returns the <paramref name="topN"/> most expansive items from the given <paramref name="collection"/>.</returns>
-    public static Task<InventoryItemsCollection> GetMostExpansiveItems(InventoryItemsCollection collection, int topN)
+    public static Task<InventoryItemsCollection> GetMostExpansiveItemsAsync(InventoryItemsCollection collection, int topN)
     {
         return Task.Run<InventoryItemsCollection>(() =>
         {
@@ -110,7 +110,7 @@ public class Analytics
     /// </summary>
     /// <param name="collection">Representing the collection of items.</param>
     /// <returns>The most expansive item or null, if the <paramref name="collection"/> is empty.</returns>
-    public static Task<InventoryItem?> GetMostExpansiveItem(InventoryItemsCollection collection)
+    public static Task<InventoryItem?> GetMostExpansiveItemAsync(InventoryItemsCollection collection)
     {
         return Task.Run<InventoryItem?>(() =>
         {
@@ -120,5 +120,46 @@ public class Analytics
             // gets the most expansive item
             return collection.OrderByDescending(x => x.PurchasePrice).First();
         });
+    }
+
+    /// <summary>
+    /// Gets the list of all items with expiration date within the <paramref name="daysThreshold"/> in the future.
+    /// </summary>
+    /// <param name="collection">Collection of items.</param>
+    /// <param name="daysThreshold">Number of days in the future from today that is considered to be in range for near expiration date.</param>
+    /// <returns>List of all items with expiration date in the near future - determined by the <paramref name="daysThreshold"/> variable.</returns>
+    public static Task<InventoryItemsCollection> NearExpiredWarrantyAsync(InventoryItemsCollection collection, int daysThreshold)
+    {
+        return Task.Run<InventoryItemsCollection>(() =>
+        {
+            if (collection.Any() == false) return [];
+
+            if (daysThreshold < 0) return [];
+
+            DateTime date = DateTime.Now.AddDays(daysThreshold);
+            InventoryItemsCollection list = [];
+
+            foreach (var item in collection)
+            {
+                if (item.HasWarranty == false || item.WarrantyExpiry.HasValue == false) continue;
+
+                if (item.WarrantyExpiry.Value < date)
+                {
+                    list.Add(item);
+                }
+            }
+
+            return list;
+        });
+    }
+
+    /// <summary>
+    /// Gets the list of all items with their warranties expired.
+    /// </summary>
+    /// <param name="collection">Collection of items.</param>
+    /// <returns>List of items with their warranties expired.</returns>
+    public static Task<InventoryItemsCollection> ExpiredWarranty(InventoryItemsCollection collection)
+    {
+        return NearExpiredWarrantyAsync(collection, 0);
     }
 }
