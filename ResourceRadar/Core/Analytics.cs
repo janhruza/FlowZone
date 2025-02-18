b695187e5791ad2c170b059fb638485d6c5d8593
Jan Hrůza <honza.hruza.d5@seznam.cz>
2025-02-18 21:15:19 +0000

    ResourceRadar: Added more methods into the Analytics class.

--- ResourceRadar/Core/Analytics.cs
+++ ResourceRadar/Core/Analytics.cs
@@ -85,4 +85,40 @@ public class Analytics
             return list;
         });
     }
+
+    /// <summary>
+    /// Gets the <paramref name="topN"/> number of the most expansive items.
+    /// </summary>
+    /// <param name="collection">Collection of items.</param>
+    /// <param name="topN">Number of items to return.</param>
+    /// <returns>Returns the <paramref name="topN"/> most expansive items from the given <paramref name="collection"/>.</returns>
+    public static Task<InventoryItemsCollection> GetMostExpansiveItems(InventoryItemsCollection collection, int topN)
+    {
+        return Task.Run<InventoryItemsCollection>(() =>
+        {
+            // logic checks
+            if (topN <= 0) return [];
+            if (collection.Count < topN) return [];
+
+            var list = collection.OrderByDescending(x => x.PurchasePrice).Take(topN).ToList();
+            return (InventoryItemsCollection)list;
+        });
+    }
+
+    /// <summary>
+    /// Gets the most expansive item from the <paramref name="collection"/> of items.
+    /// </summary>
+    /// <param name="collection">Representing the collection of items.</param>
+    /// <returns>The most expansive item or null, if the <paramref name="collection"/> is empty.</returns>
+    public static Task<InventoryItem?> GetMostExpansiveItem(InventoryItemsCollection collection)
+    {
+        return Task.Run<InventoryItem?>(() =>
+        {
+            // logic checks
+            if (collection.Any() == false) return null;
+            
+            // gets the most expansive item
+            return collection.OrderByDescending(x => x.PurchasePrice).First();
+        });
+    }
 }
