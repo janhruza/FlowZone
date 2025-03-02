bbd97b70b82d6a6adbedc1c2b1b0afd709691627
Jan Hrůza <honza.hruza.d5@seznam.cz>
2025-03-02 19:27:08 +0000

    ResourceRadar: Using directives adjustment

--- ResourceRadar/MainWindow.xaml.cs
+++ ResourceRadar/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Media.TextFormatting;
 using FZCore;
 using ResourceRadar.Core.Authentication;
 using ResourceRadar.Pages;
