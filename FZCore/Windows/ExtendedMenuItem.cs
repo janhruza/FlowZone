d2d18526950144b588d96d50e4539c9d5d9eb0b6
Jan Hrůza <honza.hruza.d5@seznam.cz>
2025-03-02 20:05:34 +0000

    FZCore: Added a window extension class WindowExtender.

---
+++ FZCore/Windows/ExtendedMenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FZCore.Windows;
+
+/// <summary>
+/// Representing a single <see cref="ExtendedMenuItem"/> structure.
+/// </summary>
+public struct ExtendedMenuItem
+{
+    /// <summary>
+    /// Representing the header text of the menu item.
+    /// </summary>
+    public string Header { get; set; }
+
+    /// <summary>
+    /// Representing the action executed when the menu item is clicked.
+    /// </summary>
+    public Action OnClick { get; set; }
+}
---
+++ FZCore/Windows/WindowExtender.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace FZCore.Windows;
+
+/// <summary>
+/// Representing an extension class to any <see cref="Window"/> class.
+/// </summary>
+public class WindowExtender
+{
+    private const int WM_SYSCOMMAND = 0x0112;
+    private const int MF_STRING = 0x00000000;
+    private const int MF_SEPARATOR = 0x00000800;
+    private const int SC_RESTORE = 0xF120; // Example: Standard system command
+
+    private IntPtr _hwnd;
+    private IntPtr _originalSystemMenu;
+    private Dictionary<int, ExtendedMenuItem> _customMenuItems;
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="WindowExtender"/> class.
+    /// </summary>
+    /// <param name="window">Target window to be extended.</param>
+    /// <exception cref="ArgumentNullException">Target widow was not fund.</exception>
+    public WindowExtender(Window window)
+    {
+        if (window == null) throw new ArgumentNullException(nameof(window));
+
+        _hwnd = new WindowInteropHelper(window).Handle;
+        _customMenuItems = new Dictionary<int, ExtendedMenuItem>();
+
+        // Hook WndProc
+        var source = HwndSource.FromHwnd(_hwnd);
+        source.AddHook(WndProc);
+
+        // Save the original system menu
+        _originalSystemMenu = GetSystemMenu(_hwnd, false);
+    }
+
+    /// <summary>
+    /// Adds new menu item to the end of the sequence.
+    /// </summary>
+    /// <param name="id">Menu item ID.</param>
+    /// <param name="header">enu item header text.</param>
+    /// <param name="onClick">On-click action for the menu item.</param>
+    public void AddMenuItem(int id, string header, Action onClick)
+    {
+        _customMenuItems[id] = new ExtendedMenuItem { Header = header, OnClick = onClick };
+        RedrawMenu();
+    }
+
+    /// <summary>
+    /// Adds new menu item to the end of the sequence.
+    /// </summary>
+    /// <param name="id">Menu item ID.</param>
+    /// <param name="menuItem">Menu item struct.</param>
+    public void AddMenuItem(int id, ExtendedMenuItem menuItem)
+    {
+        _customMenuItems[id] = menuItem;
+        RedrawMenu();
+    }
+
+    /// <summary>
+    /// Removes the menu item with the given <paramref name="id"/> from the custom menu items list.
+    /// </summary>
+    /// <param name="id">ID of the target item.</param>
+    public void RemoveMenuItem(int id)
+    {
+        if (_customMenuItems.ContainsKey(id))
+        {
+            _customMenuItems.Remove(id);
+            RedrawMenu();
+        }
+    }
+
+    /// <summary>
+    /// Restores the default system menu.
+    /// </summary>
+    public void RestoreDefaultMenu()
+    {
+        GetSystemMenu(_hwnd, true); // Restore the original menu
+        _customMenuItems.Clear();
+    }
+
+    private void RedrawMenu()
+    {
+        // Clear the current menu and restore the default system menu
+        GetSystemMenu(_hwnd, true);
+
+        IntPtr systemMenu = GetSystemMenu(_hwnd, false);
+
+        // Add a separator at the top
+        InsertMenu(systemMenu, 0, MF_SEPARATOR, 0, string.Empty);
+
+        // Insert custom items at their respective positions based on IDs
+        foreach (var menuItem in _customMenuItems.OrderBy(m => m.Key))
+        {
+            InsertMenu(systemMenu, menuItem.Key, MF_STRING, menuItem.Key, menuItem.Value.Header);
+        }
+    }
+
+
+    private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+    {
+        if (msg == WM_SYSCOMMAND)
+        {
+            int commandId = wParam.ToInt32();
+            if (_customMenuItems.TryGetValue(commandId, out var menuItem))
+            {
+                menuItem.OnClick?.Invoke();
+                handled = true;
+            }
+        }
+
+        return IntPtr.Zero;
+    }
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    private static extern bool AppendMenu(IntPtr hMenu, int uFlags, int uIDNewItem, string lpNewItem);
+
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    private static extern bool InsertMenu(IntPtr hMenu, int uPosition, int uFlags, int uIDNewItem, string lpNewItem);
+}
