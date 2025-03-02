54c5ebd032c67e4670983b0794cd1d70cd5a2c8e
Jan Hrůza <honza.hruza.d5@seznam.cz>
2025-03-02 21:18:20 +0000

    Added a new project: PassFort

---
+++ PassFort/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using FZCore;
+
+namespace PassFort;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        // set system style
+        // including drawing of system-rendered controls
+        FZCore.Core.SetApplicationTheme(FZThemeMode.System);
+
+        MainWindow mw = new MainWindow();
+        MainWindow = mw;
+        MainWindow.Show();
+    }
+
+    private void Application_Exit(object sender, ExitEventArgs e)
+    {
+        return;
+    }
+}
+
---
+++ PassFort/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace PassFort;
+
+/// <summary>
+/// Repreenting the main application window.
+/// </summary>
+public partial class MainWindow : Window
+{
+    /// <summary>
+    /// Ctreates a new instance of the <see cref="MainWindow"/> class.
+    /// </summary>
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    #region Static code
+
+    private static MainWindow? _instance;
+
+    /// <summary>
+    /// Representing the current instance of the <see cref="MainWindow"/> class.
+    /// </summary>
+    public static MainWindow Instance => _instance ??= new MainWindow();
+
+    #endregion
+
+    private void miClose_Click(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+}
