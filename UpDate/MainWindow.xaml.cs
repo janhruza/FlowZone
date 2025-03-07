8fff33fae3982268a6c130a35e41e635ff7a5da6
Jan Hrůza <honza.hruza.d5@seznam.cz>
2025-03-07 20:24:31 +0000

    New project added: UpDate

---
+++ UpDate/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace UpDate;
+
+/// <summary>
+/// Representing the main application window class.
+/// This class derives from the <see cref="Window"/> class.
+/// </summary>
+public partial class MainWindow : Window
+{
+    /// <summary>
+    /// Creates a new instance of the <see cref="MainWindow"/> class with default parameters.
+    /// </summary>
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
