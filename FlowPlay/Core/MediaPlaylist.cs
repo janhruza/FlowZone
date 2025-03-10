adf4d9030ba491a03d1c46fe8789ff5d1cc301ef
Jan Hrůza <honza.hruza.d5@seznam.cz>
2025-03-10 22:36:30 +0000

    FlowPlay: MediaPlaylist class extended.

--- FlowPlay/Core/MediaPlaylist.cs
+++ FlowPlay/Core/MediaPlaylist.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using FZCore;
 
 namespace FlowPlay.Core
 {
@@ -25,5 +27,45 @@ namespace FlowPlay.Core
         /// Representing the list of all files in this playlist.
         /// </summary>
         public List<string> Tracks { get; set; }
+
+        /// <summary>
+        /// Adds a track into the playlist.
+        /// </summary>
+        /// <param name="fileName">Path to the media file.</param>
+        /// <returns>True, if item was added, otherwise false.</returns>
+        public bool Add(string fileName)
+        {
+            if (File.Exists(fileName) == false)
+            {
+                Log.Error($"Target file \'{fileName}\' was not found.", nameof(Add));
+                return false;
+            }
+
+            this.Tracks.Add(fileName);
+            return true;
+        }
+
+        /// <summary>
+        /// Removes a track from the playlist.
+        /// </summary>
+        /// <param name="fileName">Path to the media file.</param>
+        /// <returns>True, if media file is removed from the playlist, otherwise false.</returns>
+        public bool Remove(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName) == true)
+            {
+                Log.Error("Target file is null.", nameof(Remove));
+                return false;
+            }
+
+            if (this.Tracks.Contains(fileName) == false)
+            {
+                Log.Error($"Target file \'{fileName}\' is not present in the playlist.", nameof(Remove));
+                return false;
+            }
+
+            this.Tracks.Remove(fileName);
+            return true;
+        }
     }
 }
