f2c5a5d581d2190a39906b68557a2a7f429435ab
Jan Hrůza <honza.hruza.d5@seznam.cz>
2025-03-17 21:25:26 +0000

    New project added: HashHive

---
+++ HashHive/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using FZCore;
+
+namespace HashHive;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        // log app startup
+        Log.AppStarted();
+
+        // create main window
+        MainWindow mw = new MainWindow();
+        MainWindow = mw;
+        MainWindow.Show();
+    }
+
+    private void Application_Exit(object sender, ExitEventArgs e)
+    {
+        // log app exit
+        Log.AppExited();
+    }
+}
+
