94c9c8b77f58e5f2a34cc44a2e31ef55ad36e5c8
Jan Hrůza <honza.hruza.d5@gmail.com>
2025-06-14 12:04:34 +0200

    Added WebView2 reference.

--- WebPeek/App.xaml.cs
+++ WebPeek/App.xaml.cs
@@ -1,14 +1,27 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using FZCore;
 
-namespace WebPeek
+namespace WebPeek;
+
+/// <summary>
+/// Representing the application class.
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private void Application_Startup(object sender, StartupEventArgs e)
     {
+        // startup initialization
+        Core.SetApplicationTheme(FZThemeMode.System);
+
+        // show main window
+        MainWindow mw = new MainWindow();
+        MainWindow = mw;
+        MainWindow.Show();
     }
 
+    private void Application_Exit(object sender, ExitEventArgs e)
+    {
+        // on app close cleanup
+        return;
+    }
 }
--- WebPeek/MainWindow.xaml.cs
+++ WebPeek/MainWindow.xaml.cs
@@ -1,24 +1,17 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace WebPeek
+namespace WebPeek;
+
+/// <summary>
+/// Representing the main window class.
+/// </summary>
+public partial class MainWindow : Window
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Initializes a new instance of the <see cref="MainWindow"/> class.
     /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
