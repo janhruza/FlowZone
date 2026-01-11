c1eb26cbec3a685466e84476e9646ebddbb2c2a3
Jan Hrůza <honza.hruza.d5@hotmail.com>
2026-01-11 23:19:56 +0100

    WinAPI class type aliases added.

--- FZCore/Win32/WinAPI.cs
+++ FZCore/Win32/WinAPI.cs
@@ -1,17 +1,20 @@
 ﻿using System.Runtime.InteropServices;
 
+using LPARAM = nint;
+using WPARAM = nint;
+using DWORD = int;
+using HWND = nint;
+
 namespace FZCore.Win32;
 
 internal static class WinAPI
 {
-    public const int WM_DESTROY = 0x0002;
+    [DllImport("dwmapi")]
+    public static extern HRESULT DwmExtendFrameIntoClientArea(HWND hwnd, ref MARGINS margins);
 
     [DllImport("dwmapi")]
-    public static extern HRESULT DwmExtendFrameIntoClientArea(nint hwnd, ref MARGINS margins);
-
-    [DllImport("dwmapi")]
-    public static extern HRESULT DwmSetWindowAttribute(nint hWnd, int dwAttribute, int[] pwAttribute, int cbSize);
+    public static extern HRESULT DwmSetWindowAttribute(HWND hWnd, DWORD dwAttribute, int[] pwAttribute, int cbSize);
 
     [DllImport("user32.dll", SetLastError = true)]
-    public static extern bool PostMessage(nint hWnd, uint Msg, nint wParam, nint lParam);
+    public static extern bool PostMessage(HWND hWnd, uint Msg, WPARAM wParam, LPARAM lParam);
 }
