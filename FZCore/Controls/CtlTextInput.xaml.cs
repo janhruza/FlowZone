27a77e6a21ebf9f2be1f431e8671c1ffa095aa7f
Jan Hrůza <honza.hruza.d5@hotmail.com>
2026-02-01 18:30:37 +0100

    Added a new text input control.

---
+++ FZCore/Controls/CtlTextInput.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows.Controls;
+
+namespace FZCore.Controls;
+
+/// <summary>
+/// Representing a custom text input control.
+/// </summary>
+public partial class CtlTextInput : UserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the CtlTextInput class.
+    /// </summary>
+    /// <remarks>This constructor sets up the control and prepares it for use. Call this constructor when
+    /// creating a new CtlTextInput control in your application.</remarks>
+    public CtlTextInput()
+    {
+        InitializeComponent();
+        txt.Clear();
+    }
+
+    /// <summary>
+    /// Gets or sets the header content of the control - <see cref="Label"/>.
+    /// </summary>
+    public object Header
+    {
+        get => lbl.Content;
+        set => lbl.Content = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the placeholder text.
+    /// </summary>
+    public string Placeholder
+    {
+        get => tbPlaceholder.Text;
+        set => tbPlaceholder.Text = value;
+    }
+
+    /// <summary>
+    /// Gets the underlying <see cref="System.Windows.Controls.TextBox"/> control associated with this instance.
+    /// </summary>
+    public TextBox TextBox => txt;
+
+    private void txt_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (tbPlaceholder is null) return;
+
+        if (txt.Text.Length == 0)
+        {
+            tbPlaceholder.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        else
+        {
+            tbPlaceholder.Visibility = System.Windows.Visibility.Collapsed;
+        }
+    }
+}
--- FZTest/Program.cs
+++ FZTest/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FZCore.Controls;
+using FZCore.Windows;
+
+using System;
 
 namespace FZTest;
 
@@ -7,6 +10,25 @@ internal class Program
     [STAThread]
     private static int Main(string[] args)
     {
+        CtlTextInput ctlInput = new CtlTextInput
+        {
+            Margin = new System.Windows.Thickness(10),
+            Width = 220
+        };
+
+        BaseWindow wnd = new BaseWindow
+        {
+            ResizeMode = System.Windows.ResizeMode.NoResize,
+            Title = "Control Test",
+            SizeToContent = System.Windows.SizeToContent.WidthAndHeight,
+            Content = ctlInput,
+            MinWidth = ctlInput.RenderSize.Width,
+            MinHeight = ctlInput.RenderSize.Height
+        };
+
+        wnd.MouseLeftButtonDown += (s, e) => wnd.DragMove();
+
+        _ = wnd.ShowDialog();
         return 0;
     }
 }
